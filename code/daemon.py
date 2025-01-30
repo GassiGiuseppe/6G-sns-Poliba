f2c36010b71a0264d2471fffc827ba11c40a1de9
GassiGiuseppe <g.gassi@studenti.poliba.it>
2025-01-30 16:39:59 +0100

    Deamon is a class now

--- code/daemon.py
+++ code/daemon.py
@@ -1,50 +1,52 @@
+# it is the real daemon 
+
 from pathlib import Path
 import time
 import yaml
 import threading
 
-QUEUE = []
-QUEUE_LOCK = threading.Lock()
+class Daemon:
 
-def tail_f(file_path):
-    # function similar to tail -f that monitorates file changes in real time 
+    def __init__(self, log_id):
+        self._QUEUE = []
+        self._QUEUE_LOCK = threading.Lock()
 
-    # with r is not blocking the file
-    with open(file_path, 'r') as file:
-        # end of file
-        file.seek(0, 2)
-        
-        while True:
-            line = file.readline()
-            
-            if not line:
-                time.sleep(0.1)
-                continue
-            
-            with QUEUE_LOCK:
-                QUEUE.append(line)
+        file_path = yaml.safe_load(open(Path('code/settings.yml')))[log_id]
+        file_path = Path(file_path)
+        self._file_path = file_path
 
+        # create the thread daemon but not start it
+        self._tail_thread = threading.Thread(target=self._tail_f, args=(self._file_path,))
+        self._tail_thread.daemon = True
 
-def print_queue():
-    i = 0
-    while True:
-        with QUEUE_LOCK:
-            if len(QUEUE)>i:
-                print(QUEUE[i])
-                i = i + 1
-                
-
-def main():
+        self._line_cursor = 0
     
-    file_path = yaml.safe_load(open(Path('code/settings.yml')))['log_file']
-    file_path = Path(file_path)
-
-    tail_thread = threading.Thread(target=tail_f, args=(file_path,))
-    tail_thread.daemon = True
-    tail_thread.start()
-
-    print_queue()
+    def start(self):
+        self._tail_thread.start()
 
 
+    def _tail_f(self, file_path):
+        # method similar to tail -f that monitorates file changes in real time 
+        # with r is not blocking the file
+        with open(file_path, 'r') as file:
+            # end of file
+            file.seek(0, 2)
+            while True:
+                line = file.readline()
+                if not line:
+                    time.sleep(0.1)
+                    continue
 
-main()
+                with self._QUEUE_LOCK:
+                    self._QUEUE.append(line)
+
+
+    def get_next_line(self):
+        while True:
+            with self._QUEUE_LOCK:
+                if len(self._QUEUE)> self._line_cursor:
+                    self._line_cursor = self._line_cursor + 1
+                    return self._QUEUE[self._line_cursor - 1].strip()
+                # debug phase
+                #if i == 28:
+                #    print(QUEUE)
--- code/main.py
+++ code/main.py
@@ -2,17 +2,26 @@ import subprocess
 import time
 from pathlib import Path
 
+from daemon import Daemon
+
 TEST = True
 
 def preparation():
 
-    daemon_path = Path('code/daemon.py')
-    subprocess.Popen(["python", daemon_path])
+    # daemon_path = Path('code/daemon.py')
+    # subprocess.Popen(["python", daemon_path])
 
     if TEST:
         # adversarial is used only in test phase
         adversarial_path = Path('code/adversarial.py')
         subprocess.Popen(["python", adversarial_path])
+
+    daemon = Daemon('log_file')
+    daemon.start()
+    for _ in range(30):
+        print(daemon.get_next_line())
+
+
 
 
 
