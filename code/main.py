13bd7b47c29e4024d08b4a6276c162dbb82ed3eb
GassiGiuseppe <g.gassi@studenti.poliba.it>
2025-02-05 17:42:20 +0100

    WIP first implementation of traci this version is pretty unstable but it is needed for some exploration

--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 from pathlib import Path
+import traci
 
 from daemon import Daemon
 
@@ -11,6 +12,7 @@ def preparation():
     # daemon_path = Path('code/daemon.py')
     # subprocess.Popen(["python", daemon_path])
 
+
     if TEST:
         # adversarial is used only in test phase
         adversarial_path = Path('code/adversarial.py')
@@ -18,16 +20,35 @@ def preparation():
 
     daemon = Daemon('log_file')
     daemon.start()
+
+    
     for _ in range(30):
         print(daemon.get_next_line())
 
 
 
 
+def omnet_starter():
+    sumoCmd = ["sumo-gui", "-c", "your_sumo_config_file.sumocfg"]
+    traci.start(sumoCmd)
+
+    step = 0
+    while step < 1000:  # Numero di step della simulazione
+        traci.simulationStep()
+        print(traci.vehicle.getIDList())
+        if "veh0" in traci.vehicle.getIDList():  # Controlla se il veicolo esiste
+            position = traci.vehicle.getPosition("veh0")
+            print(f"Step {step}: Posizione di veh0 -> {position}")
+        step += 1
+
+    traci.close()
+
+
 
 def main():
-    
-    preparation()
+    print('hello')
+    # preparation()
+    omnet_starter()
 
     
 
