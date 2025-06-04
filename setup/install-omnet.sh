26ffd4c6b2531e68fa975d23c4d5836e87857acf
GassiGiuseppe <93033755+GassiGiuseppe@users.noreply.github.com>
2025-06-04 17:12:57 +0200

    Update install-omnet.sh
    correction of the location

--- setup/install-omnet.sh
+++ setup/install-omnet.sh
@@ -1,4 +1,4 @@
-cd /home/$SUDO_USER
+cd /home/$SUDO_USER/workspace/
 curl -L -O https://github.com/omnetpp/omnetpp/releases/download/omnetpp-5.6/omnetpp-5.6-src-linux.tgz
 tar -xvzf ./omnetpp-5.6-src-linux.tgz
 cd ./omnetpp-5.6 
@@ -8,8 +8,8 @@ sudo -u $SUDO_USER bash -c "export PATH=/home/$SUDO_USER/workspace/omnetpp-5.6.2
 
 
 # give the user the correct right over the files
-chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/omnetpp-5.6.2
-chmod -R 775 /home/$SUDO_USER/omnetpp-5.6.2
+chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/workspace/omnetpp-5.6.2
+chmod -R 775 /home/$SUDO_USER/workspace/omnetpp-5.6.2
 
 # does the command to compile 
 sudo -u $SUDO_USER bash -c -i "
