9c4e82121fccf25901be4445f493ffbe27e9c49b
GassiGiuseppe <93033755+GassiGiuseppe@users.noreply.github.com>
2025-06-04 17:43:02 +0200

    Update install-omnet.sh
    path correction

--- setup/install-omnet.sh
+++ setup/install-omnet.sh
@@ -1,20 +1,20 @@
-cd /home/$SUDO_USER/workspace/
+cd /home/$SUDO_USER/
 curl -L -O https://github.com/omnetpp/omnetpp/releases/download/omnetpp-5.6/omnetpp-5.6-src-linux.tgz
 tar -xvzf ./omnetpp-5.6-src-linux.tgz
 cd ./omnetpp-5.6 
 
 # 
-sudo -u $SUDO_USER bash -c "export PATH=/home/$SUDO_USER/workspace/omnetpp-5.6.2/bin:\$PATH"
+sudo -u $SUDO_USER bash -c "export PATH=/home/$SUDO_USER/omnetpp-5.6.2/bin:\$PATH"
 
 
 # give the user the correct right over the files
-chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/workspace/omnetpp-5.6.2
-chmod -R 775 /home/$SUDO_USER/workspace/omnetpp-5.6.2
+chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/omnetpp-5.6.2
+chmod -R 775 /home/$SUDO_USER/omnetpp-5.6.2
 
 # does the command to compile 
 sudo -u $SUDO_USER bash -c -i "
-. /home/$SUDO_USER/workspace/omnetpp-5.6.2/setenv && 
-cd /home/$SUDO_USER/workspace/omnetpp-5.6.2 && 
+. /home/$SUDO_USER/omnetpp-5.6.2/setenv && 
+cd /home/$SUDO_USER/omnetpp-5.6.2 && 
 ./configure && 
 make"
 
