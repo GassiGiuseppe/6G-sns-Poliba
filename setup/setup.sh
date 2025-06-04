cb0c8b3cf619de54b8d86a4577e2bb669e2b20ed
GassiGiuseppe <93033755+GassiGiuseppe@users.noreply.github.com>
2025-06-04 19:07:31 +0200

    Update setup.sh
    typo

--- setup/setup.sh
+++ setup/setup.sh
@@ -56,14 +56,14 @@ SUMO_HOME="$WORKSPACE/sumo-1.6.0"
 
 # Modificare .bashrc per la persistenza
 BASHRC_PATH="/home/$SUDO_USER/.bashrc"
-sudo -u $SUDO_USER bash -c "echo 'export PATH=\$PATH:$WORKSPACE/omnetpp-5.6.2/bin' >> $BASHRC_PATH"
+sudo -u $SUDO_USER bash -c "echo 'export PATH=\$PATH:$WORKSPACE/omnetpp-5.6/bin' >> $BASHRC_PATH"
 sudo -u $SUDO_USER bash -c "echo 'export PATH=\$PATH:$WORKSPACE/sumo-1.6.0/bin' >> $BASHRC_PATH"
 sudo -u $SUDO_USER bash -c "echo 'export PATH=\$PATH:/root/miniforge3/condabin' >> $BASHRC_PATH"
 sudo -u $SUDO_USER bash -c "echo 'export SUMO_HOME=$WORKSPACE/sumo-1.6.0' >> $BASHRC_PATH"
 sudo -u $SUDO_USER bash -c "echo 'export GDK_BACKEND=x11' >> $BASHRC_PATH"
 
 # Applicare le modifiche immediatamente nella shell corrente dell'utente
-sudo -u $SUDO_USER bash -c "export PATH=\$PATH:$WORKSPACE/omnetpp-5.6.2/bin"
+sudo -u $SUDO_USER bash -c "export PATH=\$PATH:$WORKSPACE/omnetpp-5.6/bin"
 sudo -u $SUDO_USER bash -c "export PATH=\$PATH:$WORKSPACE/sumo-1.6.0/bin"
 sudo -u $SUDO_USER bash -c "export PATH=\$PATH:/root/miniforge3/condabin"
 sudo -u $SUDO_USER bash -c "export SUMO_HOME=$WORKSPACE/sumo-1.6.0"
