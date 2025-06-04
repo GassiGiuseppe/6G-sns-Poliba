4be5c7a37a543f9cef5e3f8782665f7611c2e16b
GassiGiuseppe <93033755+GassiGiuseppe@users.noreply.github.com>
2025-06-04 19:15:01 +0200

    Update vscode.sh
    typo

--- setup/vscode.sh
+++ setup/vscode.sh
@@ -1,4 +1,4 @@
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null && \
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null && \
 sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' && \
 apt update && \
 apt install -y code
