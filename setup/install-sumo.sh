4e110a9aa5e6b199fc0a722861d9aa2b0eeb3297
GassiGiuseppe <93033755+GassiGiuseppe@users.noreply.github.com>
2025-06-04 17:47:24 +0200

    Update install-sumo.sh
    cleaning

--- setup/install-sumo.sh
+++ setup/install-sumo.sh
@@ -1,6 +1,7 @@
 cd /home/$SUDO_USER
 curl -L -O https://sumo.dlr.de/releases/1.6.0/sumo-src-1.6.0.tar.gz
 tar -xzvf sumo-src-1.6.0.tar.gz 
+rm sumo-src-1.6.0.tar.gz 
 cd ./sumo-1.6.0
 mkdir build/cmake-build && cd build/cmake-build
 cmake ../..
